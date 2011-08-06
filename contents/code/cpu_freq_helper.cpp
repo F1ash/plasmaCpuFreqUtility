49a052f14fbe8d66fd473269943c68cd99e12cfe
Flash <Flash@localhost.localdomain>
2011-08-06 21:03:55 +0400

    some fixes;

--- contents/code/cpu_freq_helper.cpp
+++ contents/code/cpu_freq_helper.cpp
@@ -22,7 +22,9 @@ ActionReply CPUFreqHelper::read(QVariantMap args)
 	QString filename = args["filename"].toString();
 	out << filename << endl;
 	QString procnumb = args["procnumb"].toString();
+	out << procnumb << endl;
 	QFile file("/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename);
+	out << "/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename << endl;
  
 	if (!file.open(QIODevice::ReadOnly)) {
 		reply = ActionReply::HelperErrorReply;
@@ -30,7 +32,8 @@ ActionReply CPUFreqHelper::read(QVariantMap args)
 		QVariantMap err;
 		err["contents"] = QString("Done");
 		reply.setData(err);
- 
+		out << "Done" << endl;
+		fileOut.close();
 		return reply;
 	};
  
@@ -39,7 +42,7 @@ ActionReply CPUFreqHelper::read(QVariantMap args)
  
 	QVariantMap retdata;
 	retdata["contents"] = contents;
-	
+	out << contents << "Success" << endl;
  
 	reply.setData(retdata);
 	file.close();
@@ -51,7 +54,7 @@ ActionReply CPUFreqHelper::write(QVariantMap args)
 {
 	ActionReply reply;
  
-	QFile fileOut("myLog");
+	QFile fileOut("/tmp/myLog");
 	if (!fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) {
 		QVariantMap err;
 		err["contents"] = QString("Done");
@@ -73,11 +76,13 @@ ActionReply CPUFreqHelper::write(QVariantMap args)
  
 	QTextStream out(&file);
 	out << parametr << endl;
-	outLog << filename << endl;
+	outLog << "/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename \
+		   << parametr << endl;
 
 	QVariantMap retdata;
-	retdata["contents"] = QString("Done");
+	retdata["contents"] = QString("Success");
 	reply.setData(retdata);
+	out << "Success" << endl;
 	file.close();
 	fileOut.close();
 	return reply;
