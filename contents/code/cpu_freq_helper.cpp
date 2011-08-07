8044603ff3e4015d1fc3da5beb6e344e42d26634
Flash <Flash@localhost.localdomain>
2011-08-07 22:11:35 +0400

    saved current state;

--- contents/code/cpu_freq_helper.cpp
+++ contents/code/cpu_freq_helper.cpp
@@ -10,30 +10,26 @@ ActionReply CPUFreqHelper::read(QVariantMap args)
 {
 	ActionReply reply;
  
-	QFile fileOut("myLog");
-	if (!fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) {
-		QVariantMap err;
-		err["contents"] = QString("Done");
-		reply.setData(err);
-		/*QMessageBox::information(0, QString("Error"), QString("Creating Log Error"), 0, 0, 0);*/
-		return reply;
-	};
-	QTextStream out(&fileOut);
 	QString filename = args["filename"].toString();
-	out << filename << endl;
 	QString procnumb = args["procnumb"].toString();
-	out << procnumb << endl;
-	QFile file("/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename);
-	out << "/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename << endl;
+	QFile file;
+	if (filename == "online") {
+	file.setFileName("/sys/devices/system/cpu/cpu" + procnumb + "/" + filename);
+	}
+	else if (filename == "possible") {
+	file.setFileName("/sys/devices/system/cpu/possible");
+	}
+	else if (filename == "present") {
+	file.setFileName("/sys/devices/system/cpu/present");
+	}
+	else {
+	file.setFileName("/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename);
+	};
  
 	if (!file.open(QIODevice::ReadOnly)) {
-		reply = ActionReply::HelperErrorReply;
-		reply.setErrorCode(file.error());
 		QVariantMap err;
-		err["contents"] = QString("Done");
+		err["contents"] = QString("default");
 		reply.setData(err);
-		out << "Done" << endl;
-		fileOut.close();
 		return reply;
 	};
  
@@ -42,11 +38,9 @@ ActionReply CPUFreqHelper::read(QVariantMap args)
  
 	QVariantMap retdata;
 	retdata["contents"] = contents;
-	out << contents << "Success" << endl;
  
 	reply.setData(retdata);
 	file.close();
-	fileOut.close();
 	return reply;
 }
 
@@ -54,18 +48,16 @@ ActionReply CPUFreqHelper::write(QVariantMap args)
 {
 	ActionReply reply;
  
-	QFile fileOut("/tmp/myLog");
-	if (!fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) {
-		QVariantMap err;
-		err["contents"] = QString("Done");
-		reply.setData(err);
-		return reply;
-	};
-	QTextStream outLog(&fileOut);
 	QString filename = args["filename"].toString();
 	QString procnumb = args["procnumb"].toString();
 	QString parametr = args["parametr"].toString();
-	QFile file("/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename);
+	QFile file;
+	if (filename != "online") {
+	file.setFileName("/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename);
+	}
+	else {
+	file.setFileName("/sys/devices/system/cpu/cpu" + procnumb + "/" + filename);
+	};
  
 	if (!file.open(QIODevice::WriteOnly)) {
 		reply = ActionReply::HelperErrorReply;
@@ -76,15 +68,11 @@ ActionReply CPUFreqHelper::write(QVariantMap args)
  
 	QTextStream out(&file);
 	out << parametr << endl;
-	outLog << "/sys/devices/system/cpu/cpu" + procnumb + "/cpufreq/scaling_" + filename \
-		   << parametr << endl;
 
 	QVariantMap retdata;
 	retdata["contents"] = QString("Success");
 	reply.setData(retdata);
-	out << "Success" << endl;
 	file.close();
-	fileOut.close();
 	return reply;
 }
 
--- contents/code/main.py
+++ contents/code/main.py
@@ -8,9 +8,6 @@ from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
 import os.path, os
 
-
-COUNT_PROC = os.sysconf('SC_NPROCESSORS_ONLN')
-
 def readCpuData(number, fileName):
 	args = {}
 	args["procnumb"] = QString(number)
@@ -60,23 +57,39 @@ def writeCpuData(number, fileName, parametr):
 		current max freq
 /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
 		current min freq
+/sys/devices/system/cpu/cpu*/online
+		enable\disable(1\0) proc, not for cpu0
+/sys/devices/system/cpu/possible
+		check possible proc (for ex.: 0-1)
+/sys/devices/system/cpu/present
+		check present proc (for ex.: 0-1)
 """
 
 def define_proc_data():
+	COUNT_PROCESSOR = os.sysconf('SC_NPROCESSORS_ONLN')
 	procData = {}
+	count = readCpuData('', 'possible').data()[QString('contents')].toString().replace('\n', '').split('-')
+	present = readCpuData('', 'present').data()[QString('contents')].toString().replace('\n', '').split('-')
+	#print len(count), COUNT_PROCESSOR, ":", [str(present[i]) for i in xrange(len(present))]
+	global COUNT_PROC
+	COUNT_PROC = max(COUNT_PROCESSOR, len(count))
 	procData['count'] = COUNT_PROC
-	procData['availableFreqs'] = {i : readCpuData(str(i), 'available_frequencies') for i in xrange(COUNT_PROC)}
+	procData['availableFreqs'] = {int(i) : readCpuData(str(i), 'available_frequencies') for i in present}
 	#print [procData['availableFreqs'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
-	procData['availableGovernors'] = {i : readCpuData(str(i), 'available_governors') for i in xrange(COUNT_PROC)}
+	procData['availableGovernors'] = {int(i) : readCpuData(str(i), 'available_governors') for i in present}
 	#print [procData['availableGovernors'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
-	procData['currentFreq'] = {i : readCpuData(str(i), 'cur_freq') for i in xrange(COUNT_PROC)}
+	procData['currentFreq'] = {int(i) : readCpuData(str(i), 'cur_freq') for i in present}
 	#print [procData['currentFreq'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
-	procData['currentGovernor'] = {i : readCpuData(str(i), 'governor') for i in xrange(COUNT_PROC)}
+	procData['currentGovernor'] = {int(i) : readCpuData(str(i), 'governor') for i in present}
 	#print [procData['currentGovernor'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
-	procData['currentMaxFreq'] = {i : readCpuData(str(i), 'max_freq') for i in xrange(COUNT_PROC)}
+	procData['currentMaxFreq'] = {int(i) : readCpuData(str(i), 'max_freq') for i in present}
 	#print [procData['currentMaxFreq'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
-	procData['currentMinFreq'] = {i : readCpuData(str(i), 'min_freq') for i in xrange(COUNT_PROC)}
+	procData['currentMinFreq'] = {int(i) : readCpuData(str(i), 'min_freq') for i in present}
 	#print [procData['currentMinFreq'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
+	""" WARNING : /sys/devices/system/cpu/cpu0/online not exist anyway """
+	present.removeAll('0')
+	procData['online'] = {int(i) : readCpuData(str(i), 'online') for i in present}
+	#print [procData['online'][i].data()[QString('contents')].toString() for i in xrange(COUNT_PROC)]
 	return procData
 
 class plasmaCpuFreqUtility(plasmascript.Applet):
@@ -114,6 +127,12 @@ class plasmaCpuFreqUtility(plasmascript.Applet):
 			self.Control.move(self.popupPosition(self.Control.size()))
 			self.Control.show()
 
+	def parametersReset(self):
+		self.Control.close()
+		self.ProcData = define_proc_data()
+		self.Control = ControlWidget(self.ProcData, self)
+		self.mouseDoubleClickEvent()
+
 class ControlWidget(Plasma.Dialog):
 	def __init__(self, procData, obj, parent = None):
 		Plasma.Dialog.__init__(self, parent)
@@ -126,7 +145,7 @@ class ControlWidget(Plasma.Dialog):
 		#self.accept.setMaximumHeight(20)
 		self.reset = QPushButton()
 		self.reset.setText("Reset")
-		self.reset.clicked.connect(self.regimeDefined)
+		self.reset.clicked.connect(self.prnt.parametersReset)
 		#self.reset.setMaximumHeight(20)
 
 		self.buttonPanel = QGridLayout()
@@ -151,11 +170,17 @@ class ControlWidget(Plasma.Dialog):
 			self.layout.addWidget(self.cpuLabel[i], 1 + i, 0)
 
 			self.cpuEnable[i] = QCheckBox()
+			if i != 0 :
+				enabled = int(self.ProcData['online'][i].data()[QString('contents')].toString().replace('\n', ''))
+				#print enabled
 			if i == 0 :
 				self.cpuEnable[i].setCheckState(Qt.Checked)
 				self.cpuEnable[i].setEnabled(False)
 			else :
-				self.cpuEnable[i].setCheckState(Qt.Checked)
+				if enabled == 1 :
+					self.cpuEnable[i].setCheckState(Qt.Checked)
+				if enabled == 0 :
+					self.cpuEnable[i].setCheckState(Qt.Unchecked)
 			self.layout.addWidget(self.cpuEnable[i], 1 + i, 1)
 
 			self.comboGovernorMenu[i] = QComboBox(self)
@@ -174,24 +199,30 @@ class ControlWidget(Plasma.Dialog):
 			self.layout.addWidget(self.comboGovernorMenu[i], 1 + i, 2)
 
 			self.comboMinFreq[i] = QComboBox(self)
+			#self.comboMinFreq[i].setMinimumContentsLength(8)
 			_availableFreqs = self.ProcData['availableFreqs'][i].data()[QString('contents')].toString().replace('\n', '')
 			availableFreqs = _availableFreqs.split(' ')
 			count = availableFreqs.count('')
 			if count > 0 : availableFreqs.removeAll('')
 			for j in availableFreqs :
+				if j == 'default' : continue
 				self.comboMinFreq[i].addItem(str(j)[:-3])
+			self.comboMinFreq[i].addItem('default')
 			currentMinFreq = self.ProcData['currentMinFreq'][i].data()[QString('contents')].toString().replace('\n', '')
 			currMinFreqIdx = availableFreqs.indexOf(currentMinFreq)
 			self.comboMinFreq[i].setCurrentIndex(currMinFreqIdx)
 			self.layout.addWidget(self.comboMinFreq[i], 1 + i, 3)
 
 			self.comboMaxFreq[i] = QComboBox(self)
+			#self.comboMaxFreq[i].setMinimumContentsLength(8)
 			_availableFreqs = self.ProcData['availableFreqs'][i].data()[QString('contents')].toString().replace('\n', '')
 			availableFreqs = _availableFreqs.split(' ')
 			count = availableFreqs.count('')
 			if count > 0 : availableFreqs.removeAll('')
 			for j in availableFreqs :
+				if j == 'default' : continue
 				self.comboMaxFreq[i].addItem(str(j)[:-3])
+			self.comboMaxFreq[i].addItem('default')
 			currentMaxFreq = self.ProcData['currentMaxFreq'][i].data()[QString('contents')].toString().replace('\n', '')
 			currMaxFreqIdx = availableFreqs.indexOf(currentMaxFreq)
 			self.comboMaxFreq[i].setCurrentIndex(currMaxFreqIdx)
@@ -199,32 +230,40 @@ class ControlWidget(Plasma.Dialog):
 
 		self.setLayout(self.layout)
 
-	def regimeDefined(self, str_ = ''):
-		print str_, '==='
-		if str_ == 'Performance' :
-			self.changeRegime('performance')
-		elif str_ == 'OnDemand' :
-			self.changeRegime('ondemand')
-		elif str_ == 'Conservative' :
-			self.changeRegime('conservative')
-		elif str_== 'PowerSave' :
-			self.changeRegime('powersave')
+	def getNewProcParemeters(self):
+		newParameters = {}
+		for i in xrange(COUNT_PROC) :
+
+			newParameters[i] = {}
+			""" WARNING : /sys/devices/system/cpu/cpu0/online not exist anyway """
+			if i != 0 : newParameters[i]['enable'] = int(self.cpuEnable[i].checkState())/2
+			newParameters[i]['regime'] = self.comboGovernorMenu[i].currentText()
+			value = self.comboMinFreq[i].currentText()
+			if value != 'default' : value += '000'
+			newParameters[i]['minfrq'] = value
+			value = self.comboMaxFreq[i].currentText()
+			if value != 'default' : value += '000'
+			newParameters[i]['maxfrq'] = value
+
+		return newParameters
 
 	def changeRegime(self, key = None):
-		args = {}
-		args['key'] = key
-		act = KAuth.Action()
-
-		act.setName('org.freedesktop.auth.cpufrequtility.write')
-		act.setHelperID('org.freedesktop.auth.cpufrequtility')
-		act.setArguments(args)
-		print act.status()
-		reply = act.execute()
-		if (reply.failed()) :
-			QMessageBox.information(self, "Error",
-					QString("KAuth returned an error code: %1 \n %2").arg(reply.errorCode()).arg(reply.errorDescription()))
-		else :
-			print reply.data(), 'reply'
+		newParameters = self.getNewProcParemeters()
+		for i in xrange(COUNT_PROC) :
+			""" WARNING : /sys/devices/system/cpu/cpu0/online not exist anyway """
+			if i != 0 :
+				print i, 'online', newParameters[i]['enable']
+				writeCpuData(i, 'online', newParameters[i]['enable'])
+				""" continue -- because files for this proc not exists already """
+				if newParameters[i]['enable'] == 0 : continue
+			""" 'default' ignored for set system defaults for current proc"""
+			#print i, 'governor', newParameters[i]['regime']
+			if newParameters[i]['regime'] != 'default' : writeCpuData(i, 'governor', newParameters[i]['regime'])
+			#print i, 'min_freq', newParameters[i]['minfrq']
+			if newParameters[i]['minfrq'] != 'default' : writeCpuData(i, 'min_freq', newParameters[i]['minfrq'])
+			#print i, 'max_freq', newParameters[i]['maxfrq']
+			if newParameters[i]['maxfrq'] != 'default' : writeCpuData(i, 'max_freq', newParameters[i]['maxfrq'])
+		self.prnt.parametersReset()
 
 def CreateApplet(parent):
 	return plasmaCpuFreqUtility(parent)
